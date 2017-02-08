f427648f3ba9ad368209c8a72f0aa2c30b5a326d
4015c17 <4015c17@toronja.ece.uprm.edu>
2017-02-08 12:36:53 -0400

    Changed the Jframe title  and changed the location and size.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myframe = new JFrame("Milton Velez");
+		JFrame myframe = new JFrame(" Milton Velez ");
 		myframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myframe.setLocation(950, 400);
 		myframe.setSize(200,200);
