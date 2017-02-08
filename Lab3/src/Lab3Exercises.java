45f9d7e4d7823c18f8206f7a0077355d4690287a
4015c17 <4015c17@toronja.ece.uprm.edu>
2017-02-08 13:08:05 -0400

    Added borders and changed there location and changed colors.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myframe = new JFrame(" Milton Velez ");
-		myframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myframe.setLocation(950, 400);
-		myframe.setSize(200,200);
-		myframe.setVisible(true);
+		JFrame myFrame = new JFrame(" Milton Velez ");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(950, 400);
+		myFrame.setSize(200,200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
+		myFrame.setVisible(true);
 	}
 }
