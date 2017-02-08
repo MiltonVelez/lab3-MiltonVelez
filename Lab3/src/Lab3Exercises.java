8dc01846413e03f6ffc67c0600c145cb9eecf985
4015c17 <4015c17@toronja.ece.uprm.edu>
2017-02-08 12:33:40 -0400

    Removed comment lines, changed the Jframe tittle to " Milton Velez", and changed the size and location.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myframe = new JFrame("Milton Velez");
 		myframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myframe.setLocation(950, 450);
+		myframe.setLocation(950, 400);
 		myframe.setSize(200,200);
 		myframe.setVisible(true);
 	}
